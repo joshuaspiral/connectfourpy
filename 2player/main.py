07c2bb938e47a19ebc33f314430378055f8ce921
Your Name <you@example.com>
2022-03-12 11:27:33 +0800

    black formatter

--- 2player/main.py
+++ 2player/main.py
@@ -1,10 +1,11 @@
 HEIGHT = 6
 WIDTH = 7
 
+
 def four_cont_subarray(arr):
     count = 0
     for i in range(0, len(arr) - 1):
-        if arr[i] == arr[i + 1] and  arr[i] != ' ':
+        if arr[i] == arr[i + 1] and arr[i] != " ":
             count += 1
             if count + 1 >= 4:
                 return arr[i]
@@ -12,8 +13,8 @@ def four_cont_subarray(arr):
             count = 0
     return False
 
+
 def check_for_win(grid):
-    print('checking for win')
     cols = [[row[i] for row in grid] for i in range(WIDTH)]
     for row in grid:
         if four_cont_subarray(row) is not False:
@@ -25,53 +26,66 @@ def check_for_win(grid):
 
     for row in range(HEIGHT - 3):
         for col in range(3, WIDTH):
-            if grid[row][col] == grid[row + 1][col - 1] == grid[row + 2][col - 2] == grid[row + 3][col - 3] and grid[row][col] != ' ':
+            if (
+                grid[row][col]
+                == grid[row + 1][col - 1]
+                == grid[row + 2][col - 2]
+                == grid[row + 3][col - 3]
+                and grid[row][col] != " "
+            ):
                 return grid[row][col]
 
     for row in range(HEIGHT - 3):
         for col in range(WIDTH - 3):
-            if grid[row][col] == grid[row + 1][col + 1] == grid[row + 2][col + 2] == grid[row + 3][col + 3]:
+            if (
+                grid[row][col]
+                == grid[row + 1][col + 1]
+                == grid[row + 2][col + 2]
+                == grid[row + 3][col + 3]
+            ):
                 return grid[row][col]
 
     return False
-  
+
 
 def handle_turn(player):
     global current_turn
     print_grid(grid)
-    col = int(input(f"Which column? From 1-{WIDTH} ")) - 1
+    col = int(input(f"{current_turn}'s turn. Which column? From 1-{WIDTH} ")) - 1
 
     for i in range(HEIGHT - 1, -1, -1):
-        if (grid[i][col]) == ' ':
+        if (grid[i][col]) == " ":
             grid[i][col] = player
             break
 
     result = check_for_win(grid)
-    if result == 'Y':
-        return 'Yellow wins'
-    elif result == 'R':
-        return 'Red wins'
-    elif result == 'DRAW':
-        return 'DRAW'
+    if result == "Y":
+        return "Yellow wins"
+    elif result == "R":
+        return "Red wins"
+    elif result == "DRAW":
+        return "DRAW"
     else:
-        if current_turn == 'Y':
-            current_turn = 'R'
+        if current_turn == "Y":
+            current_turn = "R"
         else:
-            current_turn = 'Y'
+            current_turn = "Y"
+
 
 def print_grid(grid):
     print()
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            print('|', end=' ')
-            print(grid[i][j], end=' ') 
-        print('|')
-    print('-'*29)
-    print(' ', end='')
-    print('   '.join([str(x) for x in range(1, WIDTH + 1)]))
+            print("|", end=" ")
+            print(grid[i][j], end=" ")
+        print("|")
+    print("-" * 29)
+    print(" ", end="")
+    print("   ".join([str(x) for x in range(1, WIDTH + 1)]))
 
-grid = [[' ' for _ in range(WIDTH)] for x in range(HEIGHT)]
-current_turn = 'Y'
+
+grid = [[" " for _ in range(WIDTH)] for x in range(HEIGHT)]
+current_turn = "Y"
 while True:
     result = handle_turn(current_turn)
     if result:
--- ai/main.py
+++ ai/main.py
@@ -1,22 +1,24 @@
-ai = 'Y'
-human = 'R'
 from math import inf
+ai = "Y"
+human = "R"
 HEIGHT = 6
 WIDTH = 7
+
 
 def find_best_move(grid):
     best_score = -inf
     best_move = (0, 0)
     for i in range(HEIGHT):
         for j in range(WIDTH):
-            if grid[i][j] == ' ': # free space available
+            if grid[i][j] == " ":  # free space available
                 grid[i][j] = ai
                 score = minimax(grid, 0, False)
-                grid[i][j] = ' '
+                grid[i][j] = " "
                 if score > best_score:
                     best_score = score
                     best_move = (i, j)
     return best_move
+
 
 def minimax(grid, depth, is_maximising):
     print(depth)
@@ -25,17 +27,17 @@ def minimax(grid, depth, is_maximising):
         return 1
     elif result == human:
         return -1
-    elif result == 'DRAW':
+    elif result == "DRAW":
         return 0
 
     if is_maximising:
         best_score = -inf
         for i in range(HEIGHT):
             for j in range(WIDTH):
-                if grid[i][j] == ' ': # free space available
+                if grid[i][j] == " ":  # free space available
                     grid[i][j] = ai
                     score = minimax(grid, depth + 1, False)
-                    grid[i][j] = ' '
+                    grid[i][j] = " "
                     if score > best_score:
                         best_score = score
         return best_score
@@ -43,26 +45,26 @@ def minimax(grid, depth, is_maximising):
         best_score = inf
         for i in range(HEIGHT):
             for j in range(WIDTH):
-                if grid[i][j] == ' ': # free space available
+                if grid[i][j] == " ":  # free space available
                     grid[i][j] = human
                     score = minimax(grid, depth + 1, False)
-                    grid[i][j] = ' '
+                    grid[i][j] = " "
                     if score < best_score:
                         best_score = score
         return best_score
 
 
-
 def four_cont_subarray(arr):
     count = 0
     for i in range(0, len(arr) - 1):
-        if arr[i] == arr[i + 1] and  arr[i] != ' ':
+        if arr[i] == arr[i + 1] and arr[i] != " ":
             count += 1
             if count + 1 >= 4:
                 return arr[i]
         else:
             count = 0
     return False
+
 
 def check_for_win(grid):
     cols = [[row[i] for row in grid] for i in range(WIDTH)]
@@ -76,16 +78,27 @@ def check_for_win(grid):
 
     for row in range(HEIGHT - 3):
         for col in range(3, WIDTH):
-            if grid[row][col] == grid[row + 1][col - 1] == grid[row + 2][col - 2] == grid[row + 3][col - 3] and grid[row][col] != ' ':
+            if (
+                grid[row][col]
+                == grid[row + 1][col - 1]
+                == grid[row + 2][col - 2]
+                == grid[row + 3][col - 3]
+                and grid[row][col] != " "
+            ):
                 return grid[row][col]
 
     for row in range(HEIGHT - 3):
         for col in range(WIDTH - 3):
-            if grid[row][col] == grid[row + 1][col + 1] == grid[row + 2][col + 2] == grid[row + 3][col + 3]:
+            if (
+                grid[row][col]
+                == grid[row + 1][col + 1]
+                == grid[row + 2][col + 2]
+                == grid[row + 3][col + 3]
+            ):
                 return grid[row][col]
 
     return False
-  
+
 
 def handle_turn(player):
     global current_turn
@@ -93,36 +106,38 @@ def handle_turn(player):
     col = int(input(f"Which column? From 1-{WIDTH} ")) - 1
 
     for i in range(HEIGHT - 1, -1, -1):
-        if (grid[i][col]) == ' ':
+        if (grid[i][col]) == " ":
             grid[i][col] = player
             break
 
     result = check_for_win(grid)
     if result == ai:
-        return 'AI  wins'
+        return "AI  wins"
     elif result == human:
-        return 'Human wins'
-    elif result == 'DRAW':
-        return 'DRAW'
+        return "Human wins"
+    elif result == "DRAW":
+        return "DRAW"
     else:
-        if current_turn == 'Y':
-            current_turn = 'R'
+        if current_turn == "Y":
+            current_turn = "R"
         else:
-            current_turn = 'Y'
+            current_turn = "Y"
+
 
 def print_grid(grid):
     print()
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            print('|', end=' ')
-            print(grid[i][j], end=' ') 
-        print('|')
-    print('-'*29)
-    print(' ', end='')
-    print('   '.join([str(x) for x in range(1, WIDTH + 1)]))
+            print("|", end=" ")
+            print(grid[i][j], end=" ")
+        print("|")
+    print("-" * 29)
+    print(" ", end="")
+    print("   ".join([str(x) for x in range(1, WIDTH + 1)]))
 
-grid = [[' ' for _ in range(WIDTH)] for x in range(HEIGHT)]
-current_turn = human 
+
+grid = [[" " for _ in range(WIDTH)] for x in range(HEIGHT)]
+current_turn = human
 while True:
     if current_turn == human:
         result = handle_turn(current_turn)
@@ -136,16 +151,16 @@ while True:
 
         result = check_for_win(grid)
         if result == ai:
-            print('AI wins')
+            print("AI wins")
             break
         elif result == human:
-            print('Human wins')
+            print("Human wins")
             break
-        elif result == 'DRAW':
-            print('DRAW')
+        elif result == "DRAW":
+            print("DRAW")
             break
         else:
-            if current_turn == 'Y':
-                current_turn = 'R'
+            if current_turn == "Y":
+                current_turn = "R"
             else:
-                current_turn = 'Y'
+                current_turn = "Y"
